593b18a79b0b5991a1621a52b1d3f8efae2ec325
MartinD-Git <martin.daigne@epfl.ch>
2025-11-06 16:07:10 +0100

    removed inner qp

--- acados_mpc_breaking/main.py
+++ acados_mpc_breaking/main.py
@@ -2,17 +2,19 @@
 
 '''
 
-Check why it comes back to zero when simu is different, this did not happeen with casadi, is it the new parameters??
+torque(N) to current (A):
+a = (1.68-0.18)/(2.8-0.07) = 0.54945054945
+b = 0.14153846153
+
+I = a*tau + b = a*r_d*tension + b
 
 '''
 
 
 import traceback
-import casadi as ca
 import numpy as np
 from tqdm import tqdm
 import time
-import matplotlib.pyplot as plt
 from acados_utils import setup_ocp_solver, mpc_step_acados
 from pcc_arm import PCCSoftArm
 from parameters import ARM_PARAMETERS, MPC_PARAMETERS, SIM_PARAMETERS
@@ -37,12 +39,6 @@ def main():
     Tf = N * SIM_PARAMETERS['dt']
     ocp_solver = setup_ocp_solver(pcc_arm, MPC_PARAMETERS, N, Tf)
 
-    # create generalized force to tendon tension solver
-    #force2tendon_solver = create_force2tendon_function(pcc_arm)
-    initial_tendon_guess = 0.1*np.ones(3*pcc_arm.num_segments)
-    lb_tendon = np.zeros(3*pcc_arm.num_segments)
-    ub_tendon = pcc_arm.max_tension*np.ones(3*pcc_arm.num_segments)
-
     # Simu loop
     with tqdm(total=num_iter*SIM_PARAMETERS['dt'], desc="MPC loop", bar_format='{l_bar}{bar}| {n:.2f}/{total_fmt} [{elapsed}<{remaining}, {postfix}]') as pbar:
         for t in range(num_iter):
@@ -55,36 +51,26 @@ def main():
                 #q_goal_value = np.vstack((xyz_circular_traj[t:t+N+1,:].T,np.zeros((2*pcc_arm.num_segments,N+1))))  # zero velocities
 
                 u0 = mpc_step_acados(ocp_solver, pcc_arm.current_state, q_goal_value, N)
-                
+
                 loop_time_1 = time.time()
+                pcc_arm.log_history(np.zeros(2*pcc_arm.num_segments), q_goal_value[:,0],u0)
 
-
-                #convert generalized forces to tensions for the motors
-                p= np.concatenate([pcc_arm.current_state, u0])
-                #tendon_solution = force2tendon_solver(x0=initial_tendon_guess, p=p, lbx=lb_tendon, ubx=ub_tendon)
-                #u_tendon = np.array(tendon_solution['x']).flatten()
-                u_tendon = u0  # placeholder if not using QP
-                initial_tendon_guess = u0
-                loop_time_2 = time.time()
-
-                pcc_arm.log_history(np.zeros(2*pcc_arm.num_segments), q_goal_value[:,0],u_tendon)
                 # apply the first control input to the real system
                 pcc_arm.next_step(u0)
 
                 pbar.update(SIM_PARAMETERS['dt'])
 
-                loop_time_3 = time.time()
+                loop_time_2 = time.time()
                 # Timing
                 mpc_time = (loop_time_1 - loop_time_0) * 1000
-                qp_time = (loop_time_2 - loop_time_1) * 1000
-                fk_time = (loop_time_3 - loop_time_2) * 1000
-                total_time = (loop_time_3 - loop_time_0) * 1000
+                fk_time = (loop_time_2 - loop_time_1) * 1000
+                total_time = (loop_time_2 - loop_time_0) * 1000
 
                 # Set the postfix with the calculated times
-                pbar.set_postfix(MPC=f'{mpc_time:.2f}ms', QP=f'{qp_time:.2f}ms', FK=f'{fk_time:.2f}ms', Total=f'{total_time:.2f}ms', refresh=True)
+                pbar.set_postfix(MPC=f'{mpc_time:.2f}ms', FK=f'{fk_time:.2f}ms', Total=f'{total_time:.2f}ms', refresh=True)
 
             except:
-                print("status:", ocp_solver.get_status())            # expect 2 in your case
+                print("status:", ocp_solver.get_status())
                 print("alpha:", ocp_solver.get_stats('alpha'))
                 print("qp_iter:", ocp_solver.get_stats('qp_iter'))
                 print("residuals:", ocp_solver.get_residuals())  
@@ -93,28 +79,6 @@ def main():
 
     print("--- %s seconds ---" % (time.time() - start_time))
     history_plot(pcc_arm,MPC_PARAMETERS['u_bound'],dottet_plotting_traj)
-
-'''def create_force2tendon_function(arm):
-    num_segments = arm.num_segments
-    J_tendon = ca.SX.zeros((3*num_segments, 2*num_segments))
-    u_tendon = ca.SX.sym('u', 3*num_segments)
-    u_generalized = ca.SX.sym('u_gen', 2*num_segments)
-    q = ca.SX.sym('x', 4*num_segments)
-    
-    for i in range(num_segments):
-        for k in range(3): #number of tendons
-            phi =q[2*i]
-            theta = q[1+2*i]
-            J_tendon[k+3*i,2*i] = -theta*arm.r_d*ca.sin(arm.sigma_k[k]-phi)
-            J_tendon[k+3*i,2*i+1] = -arm.r_d*ca.cos(arm.sigma_k[k]-phi)
-
-    # Formulate the QP:
-    objective = ca.sumsqr(J_tendon.T @ u_tendon - u_generalized)
-
-    qp   = {'x': u_tendon, 'p': ca.vertcat(q, u_generalized), 'f': objective}
-    opts = {'print_time': False, 'printLevel': 'none'}
-    solver = ca.qpsol('force2tendon', 'qpoases', qp, opts)
-    return solver'''
 
 if __name__ == "__main__":
     main()
